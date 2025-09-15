686fc8023fc82f38a2dcf60232346c920bd96d39
paul@iqmo.com <paul@iqmo.com>
2025-09-15 07:22:11 -0400

    test: Mark test xfail

--- tests/fast/numpy/test_numpy_new_path.py
+++ tests/fast/numpy/test_numpy_new_path.py
@@ -7,11 +7,10 @@ import numpy as np
 import duckdb
 from datetime import timedelta
 import pytest
-import pandas  # https://github.com/duckdb/duckdb-python/issues/48
 
 
 class TestScanNumpy(object):
-    @pytest.mark.xfail(sys.version_info[:2] == (3, 14), reason="Fails when testing without pandas https://github.com/duckdb/duckdb-python/issues/48")
+    @pytest.mark.skipif(sys.version_info[:2] == (3, 14), reason="Fails when testing without pandas https://github.com/duckdb/duckdb-python/issues/48")
     def test_scan_numpy(self, duckdb_cursor):
         z = np.array([1, 2, 3])
         res = duckdb_cursor.sql("select * from z").fetchall()
